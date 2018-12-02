59b96cdf14360d52d141e9914f1dfe42416a586f
BeanLiu <njueebeanliu@hotmail.com>
2018-12-02 18:12:24 +0800

    根据flann结果进行比较。当前我的回溯方法可能有误。结果不正确。

--- kdtree/kdtree.cpp
+++ kdtree/kdtree.cpp
@@ -1,23 +1,121 @@
-#include "kdtree.h"
 #include <ctime>
 #include <vector>
 #include "time_utility.h"
+#include "kdtree.h"
+#include <flann/flann.hpp>
+using namespace flann;
 
-int main()
+constexpr int data_size = 6000;
+constexpr int query_size = 1000;
+constexpr int nn = 2;
+
+using ValType = DataType<double, nn>;
+using ValMemType = ValType::data_type;
+
+std::pair<std::vector<ValMemType>, std::vector<ValMemType>> GenerateData()
 {
-	using ValType = DataType<double, 3>;
 	srand(std::time(nullptr));
-	std::vector<ValType> test_data(1000000);
+	std::vector<ValMemType> test_data(data_size);
 	for (int i = 0; i < test_data.size(); ++i)
 	{
-		for (int dim = 0; dim < ValType::dimensions; ++dim)
+		for (int dim = 0; dim < nn; ++dim)
 		{
 			test_data[i][dim] = rand() % 1000;
 		}
 	}
+
+	std::vector<ValMemType> query_data(query_size);
+	for (int i = 0; i < query_data.size(); ++i)
+	{
+		for (int dim = 0; dim < nn; ++dim)
+		{
+			query_data[i][dim] = rand() % 1000;
+		}
+	}
+	return std::make_pair(test_data, query_data);
+}
+
+std::vector<ValType> mine_check(const std::vector<ValMemType>& test_data, const std::vector<ValMemType>& query_data)
+{
 	Timer<> timer;
 	KdTree<ValType> root(test_data.data(), test_data.size());
 	timer.EndTimer("TIME FOR KDTREE BUILDING: ");
+
+	timer.StartTimer();
+	std::vector<ValType> ret;
+	for (int i = 0; i < query_data.size(); ++i)
+	{
+		auto test = root.Query(query_data[i]);
+		ret.push_back(test.first->val);
+	}
+	timer.EndTimer("TIME FOR KDTREE QUERYING: ");
+
+	return ret;
+}
+
+
+std::vector<ValType> run_flann(const std::vector<ValMemType>& test_data, const std::vector<ValMemType>& query_data)
+{
+	Matrix<float> dataset(new float[data_size*nn], data_size, nn);
+	for (int i = 0; i < test_data.size(); ++i)
+		for (int dim = 0; dim < ValType::dimensions; ++dim)
+			dataset[i][dim] = test_data[i][dim];
+
+	Matrix<float> query(new float[query_size*nn], query_size, nn);
+	for (int i = 0; i < query_data.size(); ++i)
+		for (int dim = 0; dim < ValType::dimensions; ++dim)
+			query[i][dim] = query_data[i][dim];
+
+	Matrix<int> indices(new int[query.rows], query.rows, 1);
+	Matrix<float> dists(new float[query.rows], query.rows, 1);
+
+	Timer<> timer;
+	Index<L2<float> > index(dataset, flann::KDTreeIndexParams(4));
+	index.buildIndex();
+	timer.EndTimer("FLANN BUILD TIME:");
+
+	timer.StartTimer();
+	index.knnSearch(query, indices, dists, 1, flann::SearchParams());
+	timer.EndTimer("FLANN QUERY TIME:");
+
+	std::vector<ValType> ret;
+	for (int i = 0; i < query_size; ++i)
+	{
+		ret.emplace_back(test_data.data(), indices[i][0]);
+	}
+
+	delete[] dataset.ptr();
+	delete[] query.ptr();
+	delete[] indices.ptr();
+	delete[] dists.ptr();
+
+	return ret;
+}
+
+int main()
+{
+	std::vector<ValMemType> test_data, query_data;
+	std::tie(test_data, query_data) = GenerateData();
+
+	std::cout << "data size: \t" << test_data.size() << std::endl;
+	std::cout << "query size: \t" << query_data.size() << std::endl;
+
+	std::vector<ValType> ret_ref = run_flann(test_data, query_data);
+	std::vector<ValType> ret = mine_check(test_data, query_data);
+
+	KdTree<ValType> root(test_data.data(), test_data.size());
+	Timer<> timer;
+	for (int i = 0; i < query_data.size(); ++i)
+	{
+		if (EuclideanDistance(ret[i], ret_ref[i]) > 1e-6)
+		{
+			std::cout << i + 1 << "-th query didn't match." << std::endl;
+			for (int j = 0; j < nn; ++j)
+				std::cout << ret[i][j] << "\t" << ret_ref[i][j] << "\t" << query_data[i][j] << std::endl;
+			root.Query(query_data[i]);
+		}
+	}
+	timer.EndTimer("KDTREE QUERY RESULT CHECK: ");
 
 	return 0;
 }
--- kdtree/kdtree.h
+++ kdtree/kdtree.h
@@ -1,25 +1,73 @@
 #pragma once
+
 #include <array>
 #include <memory>
 #include <algorithm>
 #include <functional>
+#include <stack>
+#include <vector>
 
 template<typename ty, int dims>
 struct DataType
 {
+	typedef std::array<ty, dims> data_type;
 	typedef ty value_type;
 	constexpr static int dimensions = dims;
 
-	std::array<ty, dims> data;
+	const data_type* data;
+	int ind;
 
-public:
-	ty& operator[](size_t pos) {
-		return data[pos];
-	}	
-	const ty& operator[](size_t pos) const {
-		return data[pos];
+	DataType() = default;
+	DataType(const DataType&) = default;
+	DataType(DataType&&) noexcept = default;
+	DataType& operator=(DataType rhs)
+	{
+		this->swap(rhs);
+		return *this;
 	}
-};
+
+	DataType(const data_type vec[], int pos)
+		:data(vec), ind(pos) {}
+public:
+	const data_type& getData() const
+	{
+		return data[ind];
+	}
+	const ty& operator[](size_t pos) const
+	{
+		return getData()[pos];
+	}
+	void swap(DataType& rhs) 
+	{
+		using std::swap;
+		if (this->data != rhs.data)
+			throw std::runtime_error("swap elements from different array is not allowed.");
+		swap(this->ind, rhs.ind);
+	}
+}; 
+
+template<typename ValType1, typename ValType2>
+double EuclideanDistance(const ValType1& p1, const ValType2& p2)
+{
+	static_assert(ValType1::dimensions == ValType2::dimensions, "dimension doesn't match.");
+	double ret = 0;
+	for (int i = 0; i < ValType1::dimensions; ++i)
+	{
+		ret += std::pow(p1[i] - p2[i], 2);
+	}
+	return std::sqrt(ret);
+}
+
+template<typename ty, int dims>
+double EuclideanDistance(const std::array<ty, dims>& p1, const DataType<ty, dims>& p2)
+{
+	double ret = 0;
+	for (int i = 0; i < dims; ++i)
+	{
+		ret += std::pow(p1[i] - p2[i], 2);
+	}
+	return std::sqrt(ret);
+}
 
 template<typename ValType>
 inline bool dim_compare(const ValType& l, const ValType& r, size_t dim)
@@ -38,7 +86,7 @@ struct KdNode
 	KdNode(KdNode* Parent, ValType Val, int Split_dim)
 		:parent(Parent), val(std::move(Val)), split_dim(Split_dim) {}
 	KdNode(KdNode* Parent, ValType Val, int Split_dim, KdNode* left, KdNode* right)
-		:parent(Parent), val(std::move(Val)), split_dim(Split_dim), children({left, right}) {}
+		:parent(Parent), val(std::move(Val)), split_dim(Split_dim), children({ left, right }) {}
 };
 
 template<typename ValType>
@@ -52,13 +100,24 @@ struct KdTree
 	KdTree(const KdTree&) = default;
 	KdTree(KdTree&&) = default;
 
-	KdTree(ValType data[], int size)
+	KdTree(const typename ValType::data_type data[], int size)
 	{
-		root = BuildKdTree(data, size);
+		std::vector<ValType> data_ref;
+		for (int i = 0; i < size; ++i)
+		{
+			data_ref.emplace_back(data, i);
+		}
+
+		root = BuildKdTree(data_ref.data(), size);
 	}
 	~KdTree()
 	{
 		ReleaseKdTree(root);
+	}
+
+	std::pair<NodeType*, double> Query(const typename ValType::data_type& item)
+	{
+		return QueryNearestNode(root, item);
 	}
 
 private:
@@ -89,7 +148,7 @@ private:
 		return split_dim;
 	}
 
-	NodeType* BuildKdTree(ValType data[], int size, NodeType* parent = nullptr)
+	NodeType* BuildKdTree(typename ValType data[], int size, NodeType* parent = nullptr)
 	{
 		//利用ChooseSplitDim排序并分割
 		if (size <= 0)
@@ -102,8 +161,15 @@ private:
 			size_t mid_split_index = size / 2;
 
 			auto new_node = new NodeType(parent, data[mid_split_index], split);
-			new_node->children[1] = BuildKdTree(data + mid_split_index + 1, mid_split_index - 1, new_node);
 			new_node->children[0] = BuildKdTree(data, mid_split_index, new_node);
+			new_node->children[1] = BuildKdTree(data + mid_split_index + 1, size - mid_split_index - 1, new_node);
+
+			//std::cout << "当前节点 " << new_node->val.ind << " , 分割维度: " << new_node->split_dim;
+			//if (new_node->children[0])
+			//	std::cout << " , 左 " << new_node->children[0]->val.ind;
+			//if (new_node->children[1])
+			//	std::cout << " , 右 " << new_node->children[1]->val.ind;
+			//std::cout << std::endl;
 			return new_node;
 		}
 	}
@@ -116,5 +182,52 @@ private:
 			ReleaseKdTree(node->children[1]);
 			delete node;
 		}
+	}
+
+	std::pair<NodeType*, double> QueryNearestNode(NodeType* tree_root, const typename ValType::data_type& value) const
+	{
+		std::stack<NodeType*> path;
+		NodeType* p = tree_root;
+		while (p)
+		{
+			path.push(p);
+			if (value[p->split_dim] < p->val[p->split_dim])
+				p = p->children[0];
+			else
+				p = p->children[1];
+		}
+		//由根开始到叶的path, 开始回溯
+		p = path.top();
+		double minDistNow = EuclideanDistance(value, p->val);
+		while (!path.empty())
+		{
+			auto current = path.top();
+			path.pop();
+
+			double currentDistNow = EuclideanDistance(value, current->val);
+			if (minDistNow > currentDistNow)
+			{
+				minDistNow = currentDistNow;
+				p = current;
+			}
+
+			double DistToSplitFace = value[current->split_dim] - current->val[current->split_dim];
+			if (minDistNow > std::abs(DistToSplitFace))
+			{
+				NodeType* min_node_subtree;
+				double min_distance_subtree = -1;
+				if (DistToSplitFace > 0 && current->children[0] != nullptr)
+					std::tie(min_node_subtree, min_distance_subtree) = QueryNearestNode(current->children[0], value);
+				else if (current->children[1] != nullptr)
+					std::tie(min_node_subtree, min_distance_subtree) = QueryNearestNode(current->children[1], value);
+				if (min_distance_subtree > 0 && minDistNow > min_distance_subtree)
+				{
+					minDistNow = min_distance_subtree;
+					p = min_node_subtree;
+				}
+			}
+		}
+
+		return std::make_pair(p, minDistNow);
 	}
 };
